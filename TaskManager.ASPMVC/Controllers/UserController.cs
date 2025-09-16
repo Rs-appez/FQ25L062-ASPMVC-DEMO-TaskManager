445ad3b2d7c46a9e6c7b663ebd8c01b9da5761a7
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-16 12:34:36 +0200

    Update ASP : Init User Register

---
+++ TaskManager.ASPMVC/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.ASPMVC.Models.User;
+
+namespace TaskManager.ASPMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        //Route : /User/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterForm form)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new Exception("Formulaire invalide");
+                //Envoyer les infos du formulaire dans la DB
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskManager.ASPMVC/Models/User/UserRegisterForm.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.ASPMVC.Models.User
+{
+    public class UserRegisterForm
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}

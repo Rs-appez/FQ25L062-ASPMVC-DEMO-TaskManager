987c17264b4a0b123d39dac2fb665c58f894a8e6
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-17 12:36:37 +0200

    Add DAL : Init UserService

---
+++ TaskManager.DAL/Mapper/Mapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.DAL.Entities;
+
+namespace TaskManager.DAL.Mapper
+{
+    internal static class Mapper
+    {
+        public static User ToUser(this IDataRecord record)
+        {
+            if(record is null) throw new ArgumentNullException(nameof(record));
+            return new User()
+            {
+                UserId = (Guid)record["UserId"],
+                Email = (string)record["Email"],
+                Password = "********",
+                Role = (string)record["Role"],
+                CreationDate = (DateTime)record["CreationDate"],
+                //La prochaine ligne concerne le test de nullité d'une donnée de DB, le null c# et le null du SQL sont différents...
+                //Si la donnée est de type DBNull, alors c'est un null c#, sinon c'est une donnée à caster.
+                DisableDate = (record["DisableDate"] is DBNull) ? null : (DateTime)record["DisableDate"]
+            };
+        }
+    }
+}
---
+++ TaskManager.DAL/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.DAL.Entities;
+using TaskManager.DAL.Mapper;
+
+namespace TaskManager.DAL.Services
+{
+    internal class UserService
+    {
+        private readonly string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TaskManager;Integrated Security=True";
+
+        public IEnumerable<User> Get()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_User_Get_All";
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return reader.ToUser();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

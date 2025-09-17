2d34d822b8d92419cb54db68f67b123be0187d1e
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-17 15:56:55 +0200

    Add BLL : Add UserService + Entity

---
+++ TaskManager.BLL/Mapper/Mapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B = TaskManager.BLL.Entities;
+using D = TaskManager.DAL.Entities;
+
+namespace TaskManager.BLL.Mapper
+{
+    internal static class Mapper
+    {
+        public static D.User ToDAL (this B.User entity)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            return new D.User() { 
+                UserId = entity.UserId,
+                Email = entity.Email,
+                Password = entity.Password,
+                Role = entity.Role.ToString(),
+                CreationDate = entity.CreationDate,
+                DisableDate = entity.DisableDate
+            };
+        }
+        public static B.User ToBLL(this D.User entity)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            return new B.User(
+                entity.UserId,
+                entity.Email,
+                entity.Password,
+                entity.Role,
+                entity.CreationDate,
+                entity.DisableDate
+                );
+        }
+    }
+}
---
+++ TaskManager.BLL/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.BLL.Entities;
+using TaskManager.BLL.Mapper;
+using D = TaskManager.DAL.Services;
+
+namespace TaskManager.BLL.Services
+{
+    public class UserService
+    {
+        private D.UserService _dalService = new D.UserService();
+
+        public IEnumerable<User> Get()
+        {
+            return _dalService.Get().Select(dal => dal.ToBLL());
+        }
+
+        public User Get(Guid userId) 
+        { 
+            return _dalService.Get(userId).ToBLL();
+        }
+
+        public Guid Insert(User entity)
+        {
+            return _dalService.Insert(entity.ToDAL());
+        }
+
+        public bool Delete(Guid userId) 
+        {
+            return _dalService.Delete(userId);
+        }
+
+        public User CheckPassword(string email, string password)
+        {
+            return _dalService.CheckPassword(email, password).ToBLL();
+        }
+    }
+}

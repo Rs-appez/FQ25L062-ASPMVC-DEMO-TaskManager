d4159308442c73409676f7219757d8a67e3ac78a
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-18 16:18:55 +0200

    Exo : 5. Entité BLL

---
+++ TaskManager.BLL/Entities/ManagedTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.BLL.Entities
+{
+    public class ManagedTask
+    {
+        public Guid TaskId { get; private set; }
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public User Creator { get; private set; }
+        public DateTime? DeadLine { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        private Guid _creatorId;
+
+        public ManagedTask(Guid taskId, string title, string? description, DateTime? deadLine, DateTime creationDate, Guid creatorId)
+        {
+            TaskId = taskId;
+            Title = title;
+            Description = description;
+            DeadLine = deadLine;
+            CreationDate = creationDate;
+            _creatorId = creatorId;
+        }
+
+        public ManagedTask(Guid taskId, string title, string? description, DateTime? deadLine, DateTime creationDate, User creator)
+        {
+            TaskId = taskId;
+            Title = title;
+            Description = description;
+            DeadLine = deadLine;
+            CreationDate = creationDate;
+            Creator = creator;
+            _creatorId = Creator.UserId;
+        }
+    }
+}
